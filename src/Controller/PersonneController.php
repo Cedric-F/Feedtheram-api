74deed50030c2259663276e6bc686fc74929f5e7
Cedric-F <cedric.fontaine@gmx.fr>
2019-11-16 02:33:26 +0100

    JSON response is now a serialized object

--- src/Controller/PersonneController.php
+++ src/Controller/PersonneController.php
@@ -3,6 +3,10 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Encoder\XmlEncoder;
+use Symfony\Component\Serializer\Serializer;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
 use App\Entity\Personne;
@@ -15,17 +19,20 @@ class PersonneController extends AbstractController
   public function index($id = 1) // En cas d'absence d'id, il prend la valeur 1
   {
 
+    $encoders = [new XmlEncoder(), new JsonEncoder()];
+    $normalizers = [new ObjectNormalizer()];
+
+    $serializer = new Serializer($normalizers, $encoders);
+
     // Récupère le répertoire (Relation) correspondant à l'entité Prenom
     $repo = $this->getDoctrine()->getRepository(Personne::class);
 
     // Récupère le tuple grâce à l'id de la requête
     $personne = $repo->find($id);
 
+    $jsonContent = $serializer->serialize($personne, 'json');
+
     // Renvoie les attributs au format JSON
-    return new Response(json_encode([
-                  "nom" => $personne->getNom(),
-                  "age" => $personne->getAge(),
-                  "type" => $personne->getType()
-              ]));
+    return new Response($jsonContent);
   }
 }
